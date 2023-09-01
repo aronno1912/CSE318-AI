153376fa2dd2aa8ebccad3bec22cb9780f2b7537
aronno1912 <debjanyaronno@gmail.com>
2023-09-01 21:01:56 +0600

    decision tree submit

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,33 +6,50 @@ import java.io.IOException;
 import java.util.*;
 public class DecisionTree
 {
-
+  /***************************    Train the tree  *******************************************************/
     public static TreeNode decisionTreeLearning(List<Map<String, String>> examples, List<String> attributes, List<Map<String, String>> parentExamples) {
-        //List<String> remainingAttributes = new ArrayList<>(attributes); // Make a copy of attributes
+
+        /**
+         * If there are no examples left, it means that no example has been observed for this combination of attribute values, and we return a default value calculated from the plurality
+         * classification of all the examples that were used in constructing the node’s parent. These
+         * are passed along in the variable parent examples.
+         * **/
         if (examples.isEmpty())
         {  // System.out.println("example empty");
             return pluralityValue(parentExamples);
         }
+        /**
+         * If the remaining examples are all positive (or all negative) [here it means same classification], then we are done: we can
+         * answer it with a classification of leaf node
+         * */
         else if (allExamplesHaveSameClassification(examples))
         {    //System.out.println("sameclass");
             TreeNode leaf = new TreeNode(null);
             leaf.classification = examples.get(0).get("classification");
             return leaf;
         }
+        /**
+         *  If there are no attributes left, but both positive and negative examples,
+         * The best we can do is return the plurality classification of the remaining example**/
         else if (attributes.isEmpty())
         {
            // System.out.println("att empty");
             return pluralityValue(examples);
 
         }
+        /**If there are some positive and some negative examples, then choose the best attribute to
+         split them. Recursive step!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         * */
         else
         {
+            //choose the most important attribute to expand the tree
             String A = argMaxImportance(attributes, examples);
             TreeNode tree = new TreeNode(A);
             List<String> remainingAttributes = new ArrayList<>(attributes); // Make a copy of attributes
             remainingAttributes.remove(A);
 
-            for (String v_k : getDistinctValues(A, examples)) {
+            for (String v_k : getDistinctValues(A, examples))
+            {   //exs is the new example set on which a new subtree will be constructed
                 List<Map<String, String>> exs = filterExamples(A, v_k, examples);
                 TreeNode subtree = decisionTreeLearning(exs, new ArrayList<>(remainingAttributes), examples);
                 tree.children.put(A + " = " + v_k, subtree);
@@ -65,7 +81,7 @@ public class DecisionTree
 
         TreeNode leaf = new TreeNode(null);
         leaf.classification = mostCommonClassification;
-        System.out.println("plu");
+        //System.out.println("plu");
         return leaf;
     }
 
@@ -84,7 +100,14 @@ public class DecisionTree
 
         return true; // All examples have the same classification
     }
-    public static Map<String, List<Map<String, String>>> splitExamplesByAttributeValue(String attribute, List<Map<String, String>> examples) {
+    /**
+     * takes an attribute name and a list of examples (data points) as input. Its purpose is to split the list of examples into
+     * sublists based on the values of the specified attribute. It returns a map where the keys are the distinct attribute values, and the
+     * corresponding values are lists of examples that have that attribute value.*/
+
+    public static Map<String, List<Map<String, String>>>
+    splitExamplesByAttributeValue(String attribute, List<Map<String, String>> examples)
+    {
         Map<String, List<Map<String, String>>> attributeValueExamplesMap = new HashMap<>();
         for (Map<String, String> example : examples) {
             String attributeValue = example.get(attribute);
@@ -152,8 +175,11 @@ public class DecisionTree
         }
         return new ArrayList<>(distinctValues);
     }
-
-    public static List<Map<String, String>> filterExamples(String attribute, String value, List<Map<String, String>> examples) {
+    //takes an attribute name, an attribute value, and a list of examples (data points) as input. Its purpose is to
+    // filter the list of examples and return a new list containing only the examples that have a specific attribute value for the specified attribute.
+    public static List<Map<String, String>>
+    filterExamples(String attribute, String value, List<Map<String, String>> examples)
+    {
         List<Map<String, String>> filteredExamples = new ArrayList<>();
 
         for (Map<String, String> example : examples) {
@@ -205,6 +231,7 @@ public class DecisionTree
     }
 
     public static List<Map<String, String>> loadDatasetFromFile(String filename) {
+        //attribute,values
         List<Map<String, String>> examples = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -248,50 +275,70 @@ public class DecisionTree
         }
     }
 
-    public static void main(String[] args) {
-        //String filePath = System.getProperty("user.dir") + "/input.txt";
-        List<Map<String, String>> examples = loadDatasetFromFile("C:\\Users\\Lotus\\AI offlines\\src\\input.txt");
-        List<String> attributes = Arrays.asList("buying", "maint", "doors", "persons", "lug_boot", "safety");
-        // Shuffle the dataset randomly
+    public static double calculateStandardDeviation(double[] values) {
+        double mean = Arrays.stream(values).average().getAsDouble();
+        double sumSquaredDiff = Arrays.stream(values)
+                .map(v -> Math.pow(v - mean, 2))
+                .sum();
+        double variance = sumSquaredDiff / values.length;
+        return Math.sqrt(variance);
+    }
+
+public static void main(String[] args) {
+    // Load the dataset from the file
+    List<Map<String, String>> examples = loadDatasetFromFile("C:\\Users\\Lotus\\AI offlines\\src\\input.txt");
+    List<String> attributes = Arrays.asList("buying", "maint", "doors", "persons", "lug_boot", "safety");
+
+    // Define the number of runs for the experiment
+    int numRuns = 20;
+
+    // Initialize arrays to store accuracy values
+     double[] accuracies = new double[numRuns];
+
+    // Repeat the experiment for the specified number of runs
+    for (int run = 0; run < numRuns; run++)
+    {
+       /**Shuffle randomly and then divide into 80% for training set and 20% for testSet*********/
         Collections.shuffle(examples);
-        // Divide the data into training and testing sets (80-20 split)
-        int splitIndex = (int) (examples.size() * 0.8);
+
+        // Determine the split index for training and testing sets
+        int splitIndex = (int) (examples.size() * 0.8); // 80% for training
+
+        // Split the dataset into training and testing sets
         List<Map<String, String>> trainingExamples = examples.subList(0, splitIndex);
         List<Map<String, String>> testingExamples = examples.subList(splitIndex, examples.size());
-        //System.out.println(testingExamples);
 
-      /************************************ Train the decision tree ***************************************************/
+        // Train the decision tree
         TreeNode root = decisionTreeLearning(trainingExamples, attributes, new ArrayList<>());
-        //printDecisionTree(root, "");
 
-//        // Classify new input data
-//        Map<String, String> inputExample = new HashMap<>();
-//        inputExample.put("buying", "vhigh");
-//        inputExample.put("maint", "vhigh");
-//        inputExample.put("doors", "2");
-//        inputExample.put("persons", "2");
-//        inputExample.put("lug_boot", "small");
-//        inputExample.put("safety", "high");
-//
-//        String predictedClassification = classifyExample(root, inputExample);
-//        System.out.println("Predicted classification: " + predictedClassification);
-
-         //Test the decision tree on testing examples
+        // Test the decision tree on testing examples
         int correctPredictions = 0;
-        for (Map<String, String> example : testingExamples) {
+        for (Map<String, String> example : testingExamples)
+        {
             String predictedClassification = classifyExample(root, example);
             String actualClassification = example.get("classification");
-            System.out.println("predicted is "+predictedClassification);
-            System.out.println("actual is "+actualClassification);
+            //System.out.println("predicted is "+predictedClassification);
+            //System.out.println("actual is "+actualClassification);
             if (predictedClassification.equals(actualClassification)) {
                 correctPredictions++;
             }
         }
 
-        // Calculate accuracy
+        // Calculate accuracy for the current run
         double accuracy = (double) correctPredictions / testingExamples.size() * 100;
-        System.out.println("Accuracy: " + accuracy + "%");
+        accuracies[run] = accuracy;
+        int prun=run+1;
+        System.out.println("Run "+prun+" complete!!!");
     }
+
+    // Calculate mean accuracy and standard deviation
+    double meanAccuracy = Arrays.stream(accuracies).average().getAsDouble();
+    double stdDeviation = calculateStandardDeviation(accuracies);
+
+    // Print the results
+    System.out.println("Mean Accuracy: " + meanAccuracy + "%");
+    System.out.println("Standard Deviation of Accuracy: " + stdDeviation);
+}
 
 
 }
